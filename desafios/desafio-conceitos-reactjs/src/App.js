8ba0a3e8538648b152837d53034124dc944c70b3
William Inoue <inoue.william@hotmail.com>
2020-05-07 00:25:20 -0300

    Desafio Conceitos ReactJS

---
+++ desafios/desafio-conceitos-reactjs/src/App.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from "react";
+import api from './services/api';
+
+import "./styles.css";
+
+function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    });
+  }, []);
+
+  async function handleAddRepository() {
+    const response = await api.post('repositories', {
+      title: 'GoStackBootCamp2020-RocketSeat',
+      url: 'https://https://github.com/WilliamInoue/GoStackBootCamp2020-RocketSeat',
+      techs: ['Node.js', 'ReactJS']
+    });
+
+    setRepositories([ ...repositories, response.data ]);
+  }
+
+  async function handleRemoveRepository(id) {
+    await api.delete(`repositories/${id}`);
+
+    const newRepositories = repositories.filter(
+      repository => repository.id !== id
+    );
+
+    setRepositories(newRepositories);
+  }
+
+  return (
+    <div>
+      <ul data-testid="repository-list">
+        {repositories.map(repository => (
+        <li key={repository.id}>
+          {repository.title}
+
+          <button onClick={() => handleRemoveRepository(repository.id)}>
+            Remover
+          </button>
+        </li>
+        ))}
+      </ul>
+
+      <button onClick={handleAddRepository}>Adicionar</button>
+    </div>
+  );
+}
+
+export default App;
